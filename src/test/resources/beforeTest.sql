a56a02c44109ff8b5c2b935b4626b99db1e3c1af
bird <mostafa_shaeri_tj@yahoo.com>
2022-07-18 09:27:52 +0430

    test data added

--- src/test/resources/beforeTest.sql
+++ src/test/resources/beforeTest.sql
@@ -4,16 +4,16 @@ TRUNCATE TABLE `rating`;
 TRUNCATE TABLE `person`;
 TRUNCATE TABLE `movie_cast`;
 
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (1, 'Movie1', 1980);
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (2, 'Movie2', 1990);
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (3, 'Movie3', 1990);
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (4, 'Movie4', 2000);
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (5, 'Movie5', 2001);
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (6, 'Movie6', 2018);
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (7, 'Movie7', 1980);
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (8, 'M8', 2000);
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (9, 'M9', 2000);
-INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES (10, 'M10', 2009);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('1', 'Movie1', 1980);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('2', 'Movie2', 1990);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('3', 'Movie3', 1990);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('4', 'Movie4', 2000);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('5', 'Movie5', 2001);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('6', 'Movie6', 2018);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('7', 'Movie7', 1980);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('8', 'M8', 2000);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('9', 'M9', 2000);
+INSERT INTO `movie`(`id`, `title`, `release_year`) VALUES ('10', 'M10', 2009);
 
 INSERT INTO `movie_genres`(`id`, `movie_id`, `genres`) VALUES (1, '1', 'sport');
 INSERT INTO `movie_genres`(`id`, `movie_id`, `genres`) VALUES (2, '2', 'romantic');
@@ -38,10 +38,10 @@ INSERT INTO `rating`(`movie_id`, `average_rate`, `votes`) VALUES ('7', 4, 3);
 INSERT INTO `rating`(`movie_id`, `average_rate`, `votes`) VALUES ('8', 5.1, 2);
 INSERT INTO `rating`(`movie_id`, `average_rate`, `votes`) VALUES ('9', 1, 1);
 
-INSERT INTO `person`(`id`, `name`, `alive`) VALUES (1, 'Alex Alexian', 1);
-INSERT INTO `person`(`id`, `name`, `alive`) VALUES (2, 'Daniel Opus', 0);
-INSERT INTO `person`(`id`, `name`, `alive`) VALUES (3, 'Salman Omid', 1);
-INSERT INTO `person`(`id`, `name`, `alive`) VALUES (4, 'Ziba Zavari', 1);
+INSERT INTO `person`(`id`, `name`, `alive`) VALUES ('1', 'Alex Alexian', 1);
+INSERT INTO `person`(`id`, `name`, `alive`) VALUES ('2', 'Daniel Opus', 0);
+INSERT INTO `person`(`id`, `name`, `alive`) VALUES ('3', 'Salman Omid', 1);
+INSERT INTO `person`(`id`, `name`, `alive`) VALUES ('4', 'Ziba Zavari', 1);
 
 INSERT INTO `movie_cast`(`id`, `movie_id`, `person_id`, `role`) VALUES (1, '1', '2', 0);
 INSERT INTO `movie_cast`(`id`, `movie_id`, `person_id`, `role`) VALUES (2, '1', '1', 1);
